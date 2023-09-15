a0bb774220853e447c28ac93bc434a3f1c45f545
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-15 21:52:45 +0900

    :fire: 一度ログイン機能をコメントアウト

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 import { clsx } from '@mantine/core'
 import { NavMenu } from './NavMenu'
-import { Login } from '../Login'
+// import { Login } from '../Login'
 
 function Navbar() {
   const router = useRouter()
@@ -12,9 +12,9 @@ function Navbar() {
       <div className="fixed z-50 flex w-fit items-start justify-between">
         <NavMenu />
       </div>
-      <div className="fixed right-2 top-2 z-50 hidden w-fit md:block">
+      {/* <div className="fixed right-2 top-2 z-50 hidden w-fit md:block">
         <Login />
-      </div>
+      </div> */}
 
       <header className="py-1">
         <div
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layout/Layout'
-import { Session } from 'next-auth'
-import { SessionProvider } from 'next-auth/react'
+// import { Session } from 'next-auth'
+// import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }: AppProps<{ session: Session }>) {
+// export default function App({ Component, pageProps }: AppProps<{ session: Session }>) {
+  export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
+    // <SessionProvider session={pageProps.session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </SessionProvider>
+    // </SessionProvider>
   )
 }
